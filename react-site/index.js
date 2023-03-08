4a20a07f3bd016ec2b809de3340f286ea4d9ffa2
Lawal Ameh <amehlawal@gmail.com>
2023-03-08 14:53:30 +0100

    created the header, footer and maincontent components

--- react-site/index.js
+++ react-site/index.js
@@ -1,28 +1,44 @@
-function Page() {
+function Header() {
+    return (
+    <header>
+            <nav>
+                <img src='./logo-react-icon.png' alt="logo" width='40px' />
+            </nav>
+   </header>
+)
+}
+function MainContent() {
 
     return (
     <div>
-        <header>
-            <nav>
-            <img src='./logo-react-icon.png' alt="logo" width='40px' />
-            </nav>
-            
-        </header>
-            
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps,including mobile apps</li>
-            </ul>
-        <footer>
-            <small>© 2023 Abuh development. All rights reserved."</small>
-        </footer>
-    </div>
-    
+       <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps,including mobile apps</li>
+        </ul>
+    </div>    
 )
+}
+
+function Footer() {
+    return (
+        <footer>
+        <small>© 2023 Abuh development. All rights reserved."</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
 
 // ReactDOM.createRoot(document.getElementById('root')).render(navbar);
