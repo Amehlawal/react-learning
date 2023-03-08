d1eaf6f5b642c267102e37d245f4f8beeb7f16d4
Lawal Ameh <amehlawal@gmail.com>
2023-03-08 14:40:45 +0100

    created header and footer

--- react-site/index.js
+++ react-site/index.js
@@ -1,17 +1,30 @@
-const page = (
-    <body>
-        <img src='./logo-react-icon.png' alt="logo" width='40px' />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps,including mobile apps</li>
-        </ul>
-    </body>
+function Page() {
+
+    return (
+    <div>
+        <header>
+            <nav>
+            <img src='./logo-react-icon.png' alt="logo" width='40px' />
+            </nav>
+            
+        </header>
+            
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps,including mobile apps</li>
+            </ul>
+        <footer>
+            <small>© 2023 Abuh development. All rights reserved."</small>
+        </footer>
+    </div>
+    
 )
+}
 
 // ReactDOM.createRoot(document.getElementById('root')).render(navbar);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(page);
+root.render(<Page />);
